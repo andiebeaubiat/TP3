0cf9bbc52d4e376a24f0089061f3eeda2e27935e
Andie <andie.beaubiat@gmail.com>
2022-11-21 15:34:34 +0100

    implematation des fuonctions sum

--- MyClass.h
+++ MyClass.h
@@ -8,6 +8,9 @@ public:
 	MyClass(); //default constructeur 
 	MyClass( float a, float b);  //custom constructor
 
+	MyClass sum(MyClass MC); // fct sum
+	MyClass sum(float val);
+
 protected:
 	float a_ = 0;
 	float b_ = 0;
--- test.cpp
+++ test.cpp
@@ -12,6 +12,12 @@
 
       MyClass b(5.0,6.0);
       cout << "object b "<< b.a() << b.b() << endl;
+
+      MyClass n = a.sum(b);
+      cout << "object b "<< n.a() << n.b() << endl;
+
+      MyClass nn = a.sum(1.0);
+      cout << "object b "<< nn.a() << nn.b() << endl;
     
       return 0;
     }
