e2a5f2df1e57142b119f708e80a4d830d220c018
Andie <andie.beaubiat@gmail.com>
2022-11-21 14:25:56 +0100

    encapsulation

---
+++ MyClass.h
@@ -0,0 +1,9 @@
+class MyClass {
+public:
+	float a(); // accessor for a_
+	float b(); // accessor for b_
+
+protected:
+	float a_ = 0;
+	float b_ = 0;
+}
---
+++ Myclass.cpp
@@ -0,0 +1,12 @@
+#include "MyClass.h"
+
+float MyClass::a(){
+  return a_;
+}
+
+float MyClass::b(){
+  return b_;
+}
+
+        
+   
